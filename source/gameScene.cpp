25247d120bc647acff7cd5155e2ec024fe29ec7b
Branko Djakovic <brankodjakovic08@gmail.com>
2019-01-06 14:31:19 +0100

    Made it so bots dont shoot at the player if they are outside of his field of view

--- source/gameScene.cpp
+++ source/gameScene.cpp
@@ -47,7 +47,7 @@ void InitGame() {
 	players.push_back(new Player());
     players[1]->SetBrain(new botBrain(*players[1]));
 	players[1]->team = !myPlayer->team;
-	players[1]->body->SetTransform(b2Vec2(-1, 0), 1);
+	players[1]->body->SetTransform(b2Vec2(-1, -3), 1);
     players[1]->input.shoot = true;
 
 	itemPool = ItemPool();
