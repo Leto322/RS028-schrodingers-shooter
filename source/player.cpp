146d188600a859543b5118a8c10a95ebe1d9379a
krcma96 <krcma96@gmail.com>
2019-01-06 17:22:52 +0100

    Fix after the merge

--- source/player.cpp
+++ source/player.cpp
@@ -31,7 +31,7 @@ Player::Player(){
     input.horizontal = 0;
 	input.shoot = false;
 	input.angle= M_PI/2;
-	equiped_weapon = new Weapon(0.0f, 0.0f, input.angle, 0.1);
+	equiped_weapon = new Pistol(0.0f, 0.0f, input.angle);
     team = false;
     see_player = false;
 
